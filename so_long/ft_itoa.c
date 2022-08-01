af7de42e53859c8725b059184cdeb290665be522
Gerard Martinez guillamon <gemartin@cbr12s2.42barcelona.com>
2022-08-02 00:57:43 +0200

    OK 125%

---
+++ so_long/ft_itoa.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gemartin <gemartin@student.42barc...>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/22 13:51:22 by gemartin          #+#    #+#             */
+/*   Updated: 2022/05/17 01:07:58 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+long int	static	ft_baseexponente(long int base, size_t exp)
+{
+	long int	nbr;
+
+	if (exp == 0)
+		return (1);
+	nbr = base;
+	while (--exp)
+		nbr *= base;
+	return (nbr);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*nbr;
+	size_t	digitos;
+
+	digitos = 1;
+	while (n / ft_baseexponente(10, digitos))
+		digitos++;
+	nbr = (char *)ft_calloc(digitos + 1, sizeof(char));
+	if (!nbr)
+		return (0);
+	while (--digitos)
+	{
+		nbr[digitos] = (char)((n % 10) + '0');
+		n /= 10;
+	}
+	nbr[digitos] = (char)((n % 10) + '0');
+	return (nbr);
+}
---
+++ so_long/key_press.c
@@ -0,0 +1,154 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move_w.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/15 22:51:33 by marvin            #+#    #+#             */
+/*   Updated: 2022/05/20 19:31:19 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+#include "mlx/mlx.h"
+
+void	move_w(t_list *d)
+{
+	int	i;
+
+	i = 0;
+	while (d->big_line[i] != 'P')
+		i++;
+	if (d->big_line[i - d->width_l] != '1'
+		&& !check_exit(d, d->big_line[i - d->width_l]))
+	{
+		d->moves++;
+		if (d->big_line[i - d->width_l] == 'C')
+			d->consum--;
+		d->big_line[i] = '0';
+		d->big_line[i - d->width_l] = 'P';
+		if (d->npccontrol == 0)
+		{
+			d->npcstart = d->npcback;
+			d->npccontrol = 1;
+		}
+		else
+		{
+			d->npcstart = d->npcbmv;
+			d->npccontrol = 0;
+		}
+	}
+	print_map(d);
+}
+
+void	move_s(t_list *d)
+{
+	int	i;
+
+	i = 0;
+	while (d->big_line[i] != 'P')
+		i++;
+	if (d->big_line[i + d->width_l] != '1'
+		&& !check_exit(d, d->big_line[i + d->width_l]))
+	{
+		d->moves++;
+		if (d->big_line[i + d->width_l] == 'C')
+			d->consum--;
+		d->big_line[i] = '0';
+		d->big_line[i + d->width_l] = 'P';
+		if (d->npccontrol == 0)
+		{
+			d->npcstart = d->npc;
+			d->npccontrol = 1;
+		}
+		else
+		{
+			d->npcstart = d->npcmv;
+			d->npccontrol = 0;
+		}
+	}
+	print_map(d);
+}
+
+void	move_a(t_list *d)
+{
+	int	i;
+
+	i = 0;
+	while (d->big_line[i] != 'P')
+		i++;
+	if (d->big_line[i - 1] != '1' && !check_exit(d, d->big_line[i - 1]))
+	{
+		d->moves++;
+		if (d->big_line[i - 1] == 'C')
+			d->consum--;
+		d->big_line[i] = '0';
+		d->big_line[i - 1] = 'P';
+		if (d->npccontrol == 0)
+		{
+			d->npcstart = d->npcleft;
+			d->npccontrol = 1;
+		}
+		else
+		{
+			d->npcstart = d->npclmv;
+			d->npccontrol = 0;
+		}
+	}
+	print_map(d);
+}
+
+void	move_d(t_list *d)
+{
+	int	i;
+
+	i = 0;
+	while (d->big_line[i] != 'P')
+		i++;
+	if (d->big_line[i + 1] != '1' && !check_exit(d, d->big_line[i + 1]))
+	{
+		d->moves++;
+		if (d->big_line[i + 1] == 'C')
+			d->consum--;
+		d->big_line[i] = '0';
+		d->big_line[i + 1] = 'P';
+		if (d->npccontrol == 0)
+		{
+			d->npcstart = d->npcright;
+			d->npccontrol = 1;
+		}
+		else
+		{
+			d->npcstart = d->npcrmv;
+			d->npccontrol = 0;
+		}
+	}
+	print_map(d);
+}
+
+int	key_press(int key, t_list *d)
+{
+	char	*moves_p;
+
+	(void)d;
+	if (key == 53)
+	{
+		mlx_destroy_window(d->mlx, d->win);
+		ft_free(d);
+	}
+	if (key == 13)
+		move_w(d);
+	if (key == 0)
+		move_a(d);
+	if (key == 1)
+		move_s(d);
+	if (key == 2)
+		move_d(d);
+	moves_p = ft_itoa(d->moves);
+	write(1, moves_p, ft_strlen(moves_p));
+	if (moves_p)
+		free(moves_p);
+	write(1, "\n", 1);
+	return (0);
+}
